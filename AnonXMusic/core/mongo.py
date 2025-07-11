1f7cc515e44275c57d32c215d43987f63edd5e84
terimakich <botxd495@gmail.com>
2025-07-11 10:58:41 +0530

    Update mongo.py

--- AnonXMusic/core/mongo.py
+++ AnonXMusic/core/mongo.py
@@ -6,7 +6,7 @@ import config
 
 from ..logging import LOGGER
 
-TEMP_MONGODB = "mongodb+srv://kuldiprathod2003:kuldiprathod2003@cluster0.wxqpikp.mongodb.net/?retryWrites=true&w=majority"
+TEMP_MONGODB = "mongodb+srv://villain:villain@cluster0.ygaks0v.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
 
 if config.MONGO_DB_URI is None:
